59b311a56f5bc842730f8045a12cb70412956112
Ayoub Kaarich <akaarich@c2r3p8.1337.ma>
2025-11-13 23:40:09 +0100

    dd

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akaarich <akaarich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 22:15:20 by akaarich          #+#    #+#             */
+/*   Updated: 2025/11/13 23:38:57 by akaarich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char *get_and_store(char *buf,int fd)
 {
-	
+	read(fd ,buf ,100);
+	return buf;
+}
+int main ()
+{
+	char buf[100];
+	int fd = open("txt.txt" ,O_RDONLY);
+	printf("%s",get_and_store(buf,fd));
 }
